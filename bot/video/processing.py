cfca6af68b9db63d596078f62a19af85bb9b66f3
Andrus <andredevise@gmail.com>
2022-11-07 07:54:00 +0300

    Improve frames' saving

--- bot/video/processing.py
+++ bot/video/processing.py
@@ -9,5 +9,5 @@ def extract_video_frames(video_path: str, output_path: str):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    file_paths = os.path.join(output_path, "%d.jpg")
+    file_paths = os.path.join(output_path, "%06d.jpg")
     ffmpeg.input(video_path).filter("fps", fps="1").output(file_paths, start_number=0).run()
