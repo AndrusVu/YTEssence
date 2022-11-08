8f3dce3152649542213b9704ec5c9348f1a39b29
Andrus <andredevise@gmail.com>
2022-11-08 22:06:00 +0300

    Add generation caption from image

--- bot/video/processing.py
+++ bot/video/processing.py
@@ -1,6 +1,14 @@
 import os
 
 import ffmpeg
+from lavis.models import load_model_and_preprocess
+from PIL import Image
+import torch
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # setup device to use
+MODEL, vis_processors, text_processors = load_model_and_preprocess(
+    "blip_caption", "large_coco", device=DEVICE, is_eval=True
+)
 
 
 def extract_video_frames(video_path: str, output_path: str):
@@ -11,3 +19,14 @@ def extract_video_frames(video_path: str, output_path: str):
 
     file_paths = os.path.join(output_path, "%06d.jpg")
     ffmpeg.input(video_path).filter("fps", fps="1").output(file_paths, start_number=0).run()
+
+
+def process_image(file_path: str) -> list[str]:
+    # preprocessing
+    raw_image = Image.open(file_path).convert("RGB")
+    image = vis_processors["eval"](raw_image).unsqueeze(0).to(DEVICE)  # preprocess image and text inputs
+
+    # common_capture = MODEL.generate({"image": image})  # generate caption using beam search  # FIXME remove it
+
+    # due to the non-determinstic nature of necleus sampling, you may get different captions.
+    return MODEL.generate({"image": image}, use_nucleus_sampling=True, num_captions=3)
